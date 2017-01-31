a576eecbb7b857a5496a0e13d2222b44ad0e41aa
kanjih <kanjih@ciandt.com>
2017-01-31 16:36:26 -0200

    Fixed this comments: Please, remove unused methods. See code review tab for more details..

--- app/src/main/java/com/kanjih/bhtourguide/to/Place.java
+++ app/src/main/java/com/kanjih/bhtourguide/to/Place.java
@@ -9,15 +9,12 @@ import java.io.Serializable;
 
 public class Place  implements Serializable {
 
-
-
     private int name;
     private int descriptionStringId = -1;
     private int smallDescription;
     private int imageResourseId = -1;
     private int imageScreenResource = -1;
     //@// TODO: 1/24/17 include map position
-
 
     public Place(int name, int descriptionStringId, int smallDescription, int imageResourseId, int imageScreenResource) {
         this.name = name;
@@ -31,39 +28,19 @@ public class Place  implements Serializable {
         return name;
     }
 
-    public void setName(int name) {
-        this.name = name;
-    }
-
     public int getDescriptionStringId() {
         return descriptionStringId;
-    }
-
-    public void setDescriptionStringId(int descriptionStringId) {
-        this.descriptionStringId = descriptionStringId;
     }
 
     public int getSmallDescription() {
         return smallDescription;
     }
 
-    public void setSmallDescription(int smallDescription) {
-        this.smallDescription = smallDescription;
-    }
-
     public int getImageResourseId() {
         return imageResourseId;
     }
 
-    public void setImageResourseId(int imageResourseId) {
-        this.imageResourseId = imageResourseId;
-    }
-
     public int getImageScreenResource() {
         return imageScreenResource;
-    }
-
-    public void setImageScreenResource(int imageScreenResource) {
-        this.imageScreenResource = imageScreenResource;
     }
 }
